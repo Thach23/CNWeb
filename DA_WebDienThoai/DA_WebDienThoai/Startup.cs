daf7b864b7eb229e0f34b37d17e5bb40290dd478
Thach23 <89183730+Thach23@users.noreply.github.com>
2021-08-26 18:27:06 +0700

    upload

---
+++ DA_WebDienThoai/DA_WebDienThoai/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DA_WebDienThoai.Startup))]
+namespace DA_WebDienThoai
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
